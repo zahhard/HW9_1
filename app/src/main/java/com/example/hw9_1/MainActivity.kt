f59384558064c57e9ae7ebf10b474f6b6473e404
zahhard <za.darardoust@gmail.com>
2022-03-08 13:48:15 +0330

    menu added

--- app/src/main/java/com/example/hw9_1/MainActivity.kt
+++ app/src/main/java/com/example/hw9_1/MainActivity.kt
@@ -2,10 +2,37 @@ package com.example.hw9_1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            R.id.item_home -> {
+                true
+            }
+            R.id.item_coming -> {
+                true
+            }
+            R.id.item_favorite -> {
+                true
+            }
+            R.id.item_profile -> {
+                true
+            }
+            else -> return super.onOptionsItemSelected(item)
+        }
     }
 }
