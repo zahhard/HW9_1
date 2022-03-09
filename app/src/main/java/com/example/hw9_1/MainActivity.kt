ff0fb8b17cdf60f836838da42d606980819f59a7
zahhard <za.darardoust@gmail.com>
2022-03-09 12:18:07 +0330

    menu exchanging with navigation handled

--- app/src/main/java/com/example/hw9_1/MainActivity.kt
+++ app/src/main/java/com/example/hw9_1/MainActivity.kt
@@ -1,10 +1,16 @@
 package com.example.hw9_1
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,9 +27,11 @@ class MainActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when(item.itemId){
             R.id.item_home -> {
+                goToComingSoon()
                 true
             }
             R.id.item_coming -> {
+                goToComingSoon()
                 true
             }
             R.id.item_favorite -> {
@@ -34,5 +42,32 @@ class MainActivity : AppCompatActivity() {
             }
             else -> return super.onOptionsItemSelected(item)
         }
+    }
+
+    private fun goToComingSoon() {
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        navController.navigate(R.id.action_homeFragment_to_commingSoonFragment)
+    }
+
+    private fun goToFavorite() {
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        navController.navigate(R.id.action_homeFragment_to_commingSoonFragment)
+    }
+
+    private fun goToProfile() {
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        navController.navigate(R.id.action_homeFragment_to_commingSoonFragment)
     }
 }
